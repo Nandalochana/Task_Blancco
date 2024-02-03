d4e93143636e3e71b9b6dc26c67df83dcfeb12cc
NandaLochana <danushkanandalochana@gmail.com>
2024-02-03 12:11:10 +0200

    Settup the command and design the structure

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>// for io
+#include <fstream> // to read the files
+
+
+
+int main(int argc, char* argv[]) {
+    std::cout << " Successfully run!" << std::endl;
+    return EXIT_SUCCESS;
+}
